49e050ac53892ad1ac5c2fe5996f460756facb39
kaithapram <kaithapram123@gmail.com>
2019-05-06 19:40:15 +0530

    update

---
+++ sw.js
@@ -0,0 +1,42 @@
+var urlsToCache = [
+    'index.html',
+    '/assets/main.css',
+'/bower_components/jquery/dist/jquery.min.js',
+'/bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+'/js/footer.js'
+];
+
+var CACHE_NAME = 'progressive-hyde-cache-v1';
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.match(event.request).then(function (response) {
+                return response || fetch(event.request).then(function (response) {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return fetch(event.request).then(function (response) {
+                cache.put(event.request, response.clone());
+                return response;
+            });
+        })
+    );
+});
